695c8861c24af404b797ed49d6585812516d3bab
crissed53 <44250064+crissed53@users.noreply.github.com>
2025-02-09 16:31:56 +0000

    update

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 set -exu
 
-apt install -y \
+DEBIAN_FRONTEND=noninteractive apt install -y \
     curl wget unzip zip bzip2 gzip tar git vim less openssh-client \
     openssh-server screen htop iotop nmap net-tools iputils-ping \
     build-essential
