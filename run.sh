1122c71179364280cc3296814e15b977931f5f24
root <crissed53@github.com>
2025-02-10 22:23:13 +0900

    update

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ set -exu
 DEBIAN_FRONTEND=noninteractive apt install -y \
     curl wget unzip zip bzip2 gzip tar git vim less openssh-client \
     openssh-server screen htop iotop nmap net-tools iputils-ping \
-    build-essential
+    build-essential python3-venv
 
 pushd ./src
 
