6fd0eb95c0d59f313f0a8972c8d59ccacc7daf7e
root <crissed53@github.com>
2025-02-10 21:55:48 +0900

    update

--- src/base.sh
+++ src/base.sh
@@ -203,6 +203,41 @@ go install github.com/jesseduffield/lazygit@latest
 EOF
 }
 
+install_docker() {
+  # TODO: make this dependent on current user
+  alias sudo=""
+
+  del_existing_package() {
+    for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc;
+      do sudo apt-get remove $pkg;
+    done
+  }
+
+  setup_docker_apt_repo() {
+    # Add Docker's official GPG key:
+    sudo apt-get update
+    sudo apt-get install ca-certificates curl
+    sudo install -m 0755 -d /etc/apt/keyrings
+    sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+    sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+    # Add the repository to Apt sources:
+    echo \
+      "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+      $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+      sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    sudo apt-get update
+  }
+
+  install_docker() {
+     sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+  }
+
+  del_existing_package
+  setup_docker_apt_repo
+  install_docker
+}
+
 main() {
   set -exu
   mkdir -p "$WORKSPACE_DIR"
